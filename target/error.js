19b926418622250897cb8b98fb4dd18701646428
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-30 09:58:15 -0300

    [ :sparkles: ] | create: sample - error.js (app::target)

---
+++ target/error.js
@@ -0,0 +1,22 @@
+var text = "Hello World" + "! :D";
+
+var neverAssigned = {};
+neverAssigned.name = "Samuel Ricardo";
+
+var tobeReassigned = {};
+
+tobeReassigned = { name: "ana" };
+tobeReassigned.name = 1;
+tobeReassigned = 0;
+tobeReassigned = { name: "ana" };
+
+// text = "123"
+// text = "123"
+// text = "123"
+// text = "123"
+
+let result = test
+  .split(",")
+  .map((letter) => letter.toUpperCase())
+  .join(".");
+console.log(result);
