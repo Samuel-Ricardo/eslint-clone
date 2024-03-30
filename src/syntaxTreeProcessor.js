a5d2d3d29775d67977047af006c13c494d23dafc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-30 10:01:40 -0300

    [ :sparkles: ] | setup: processor - syntax tree (app)

---
+++ src/syntaxTreeProcessor.js
@@ -0,0 +1,23 @@
+export default class SyntaxTreeProcessor {
+  #filePath;
+  #errors = new Map();
+  #variables = new Map();
+  #messages = {
+    singleQuote: () => "use single quotes instead of double quotes",
+    useConst: (variableKind) => `use "const" instead of "${variableKind}"`,
+    useLet: (variableKind) => `use "let" instead of "${variableKind}"`,
+  };
+  #stages = {
+    declaration: "declaration",
+    expressionDeclaration: "expressionDeclaration",
+  };
+  #variableKinds = {
+    const: "const",
+    let: "let",
+    var: "var",
+  };
+
+  constructor(filePath) {
+    this.#filePath = filePath;
+  }
+}
